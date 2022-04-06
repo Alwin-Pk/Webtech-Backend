98b363d3cee5b0bfc1c365ba637cd1ae8c5bac50
SlywxlF <slywolf2750@gmail.com>
2022-04-06 14:46:16 +0200

    Test Commit

---
+++ src/main/java/de/htw/webtechprojekt/WebtechProjektApplication.java
@@ -0,0 +1,13 @@
+package de.htw.webtechprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtechProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtechProjektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htw/webtechprojekt/WebtechProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htw.webtechprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtechProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
