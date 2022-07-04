d92c691d3336b171a7ee0b15cd37ee37f414bc6e
SlywxlF <slywolf2750@gmail.com>
2022-07-04 05:56:03 +0200

    Backend Test -> Bug fix attempt #1

--- src/main/java/de/htw/backend/web/GuestRestController.java
+++ src/main/java/de/htw/backend/web/GuestRestController.java
@@ -39,17 +39,18 @@ public class GuestRestController
     }
 
     @PostMapping(path = "/api/v1/guests")
-    public ResponseEntity<Void> createGuest(@Valid @RequestBody GuestManipulationRequest request) throws URISyntaxException
+    public ResponseEntity<Void> createGuest(@RequestBody GuestManipulationRequest request) throws URISyntaxException
     {
-       var valid = validate(request);
-       if (valid)
-       {
+//        @Valid
+//       var valid = validate(request);
+//       if (valid)
+//       {
            var guest = guestService.create(request);
            URI uri = new URI("/api/v1/guests/" + guest.getId());
            return ResponseEntity.created(uri).build();
-       } else {
-           return ResponseEntity.badRequest().build();
-       }
+//       } else {
+//           return ResponseEntity.badRequest().build();
+//       }
     }
 
     @DeleteMapping(path = "/api/v1/guests/{id}")
